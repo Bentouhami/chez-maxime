6e028f20cd496fa1156f125c293f87ee23b5e3d5
Faisal Bentouhami <bentouhami.faisal@gmail.com>
2024-08-17 23:14:25 +0200

    fixed Decimal .toNumber removed from price field
    Took 4 minutes

--- src/ui/products/ProductItem.tsx
+++ src/ui/products/ProductItem.tsx
@@ -35,7 +35,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
                 {product.description}
             </p>
             <p className="my-2 text-xl text-gray-700 p-1 line-clamp-1">
-                {product.price.toNumber()} €
+                {product.price} €
             </p>
             <Link
                 className="text-xl bg-purple-700 hover:bg-purple-800 w-full block text-center p-1 text-white rounded-lg"
